f3eb5c0f970253eaee4900869fcb74250a978e6b
harryrgrove <36138526+harryrgrove@users.noreply.github.com>
2020-01-03 04:38:01 +0000

    Update rawDataUpdate.py

--- rawDataUpdate.py
+++ rawDataUpdate.py
@@ -12,6 +12,10 @@ def rawDataUpdate():
     from time import time
     from understat import Understat
     from collections import OrderedDict
+    from getPlayer import getPlayer, getPosition, playerNames
+    import numpy as np
+    import csv
+    import requests
 
     """
     The following loop writes 4 seperate json dicts:
@@ -36,18 +40,70 @@ def rawDataUpdate():
                 teams.update([matchInfo['h']['title'],
                               matchInfo['a']['title']])  # Iterates through matches to add teams to teams set
 
-            matchDB, playerDB, teamDB, fixtureDB = OrderedDict(), {team: {} for team in teams}, {team: {} for team in
-                                                                                                 teams}, {
-                                                   team: OrderedDict() for team in
-                                                   teams}  # Sets up starting conditions for the creation of database dicts
+            squads = {team: [] for team in teams}
 
-            for i, team in enumerate(teams):
-                fixtures = await understat.get_team_fixtures(team, 2019)
-                for fixture in fixtures:
-                    print(fixture)
-                    fixtureDB[team][int(fixture['id'])] = {'loc': fixture['side'], 'opponent':
-                        fixture['a' * (fixture['side'] == 'h') + 'h' * (fixture['side'] == 'a')]['title']}
-                print('\n' * 20 + str(i + 1) + '/20 fixtures updated')
+            def baselineBonus(understat):
+                poisition = getPosition(understat, 'understat')
+                mean, sd = 0, 0
+                GWStats, baselineBPS = [], []
+                name = getPlayer(understat, 'understat', 'name').replace(' ', '_', 1)
+                exceptions = {619: 'Sergio_Ag%C3%BCero', 3293: 'Lucas_Rodrigues Moura da Silva',
+                              1040: 'José Ángel_Esmorís Tasende', 1208: 'Felipe Anderson_Pereira Gomes',
+                              5741: 'Jorge Luiz_Frello Filho', 1663: 'Johann Berg_Gudmundsson',
+                              1676: 'David_Luiz Moreira Marinho', 1724: 'Isaac_Success Ajayi',
+                              2132: 'José Ignacio_Peleteiro Romallo', 2379: 'João Pedro Cavaco_Cancelo',
+                              2383: 'André Filipe_Tavares Gomes', 2446: 'Daniel_Ceballos Fernández',
+                              3303: 'Ricardo Domingos_Barbosa Pereira', 3422: 'João Filipe Iria_Santos Moutinho',
+                              5245: 'Bernardo_Fernandes da Silva Junior',
+                              3635: 'Bernardo Mota_Veiga de Carvalho e Silva', 5543: 'Gabriel Fernando_de Jesus',
+                              5675: 'Ismaïla_Sarr', 579: 'Nathan_Aké', 6122: 'Douglas Luiz_Soares de Paulo',
+                              614: 'Fernando_Luiz Rosa', 6817: 'Frederico_Rodrigues de Paula Santos',
+                              6853: 'Rúben Diogo_da Silva Neves', 6856: 'Rúben Gonçalo_Silva Nascimento Vinagre',
+                              700: 'Willian_Borges Da Silva', 7752: 'Gabriel Teodoro_Martinelli Silva',
+                              87: 'Joelinton Cássio_Apolinário de Lira'}
+                if understat in exceptions:
+                    name = exceptions[understat]
+                url = 'https://raw.githubusercontent.com/vaastav/Fantasy-Premier-League/master/data/2019-20/players/' + name + '_' + str(
+                    getPlayer(understat, 'understat', 'fpl')) + '/gw.csv'
+                r = requests.get(url)
+                text = r.iter_lines()
+                reader = csv.reader(text, delimiter=',')
+                for line in text:
+                    if str(line) == "b'404: Not Found'":
+                        if understat in exceptions:
+                            print(understat)
+                        return (0, 0)
+                    GWStats.append(str(line)[2:-1].split(","))
+                for row in GWStats[1:]:
+                    if row != ['']:
+                        if int(row[12]) >= 60:
+                            baselineBPS.append(int(row[2]) - int(row[8]) * (
+                                        12 * (poisition == 'DEF' or poisition == 'GKP') + 18 * (
+                                            poisition == 'MID') + 24 * (poisition == 'FOR')) - int(row[0]) * 9 - int(
+                                row[3]) * 12 * (poisition == 'DEF' or poisition == 'MID'))
+                if len(baselineBPS) < 1:
+                    return (0, 0)
+                return (np.mean(baselineBPS), np.std(baselineBPS))
+
+            for team in teams:
+                for playerInfo in [i for i in playerNames if i['team'] == team]:
+                    squads[team].append(playerInfo['understat'])
+            baselineDB = {team: {} for team in teams}
+            count = 0
+            for team, squad in squads.items():
+                count += 1
+                print('\n' * 20)
+                print(str(count) + '/20: ' + team + ' bonus point data collecting...')
+                for player in squad:
+                    if player:
+                        baselineDB[team][int(player)] = baselineBonus(player)
+            print(baselineDB)
+
+            with open('baselineDB.json', 'w') as fp:
+                json.dump(baselineDB, fp)
+
+            matchDB, playerDB, teamDB = OrderedDict(), {team: {} for team in teams}, {team: {} for team in
+                                                                                      teams}  # Sets up starting conditions for the creation of database dicts
 
             totalProgress, countProgress, startTime = len(
                 results), 0, time()  # Initialises timing variables to give time estimate
@@ -106,8 +162,6 @@ def rawDataUpdate():
                 json.dump(matchDB, fp)
             with open('playerDB.json', 'w') as fp:
                 json.dump(playerDB, fp)
-            with open('fixtureDB.json', 'w') as fp:
-                json.dump(fixtureDB, fp)
             with open('teamDB.json', 'w') as fp:
                 json.dump(teamDB, fp)  # Compiles each dict as a JSON file to be used in teamFDR.py
             with open('results.json', 'w') as fp:
