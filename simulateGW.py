7e30eafa78654b2d9f0babad12c7c10c3785829a
harryrgrove <36138526+harryrgrove@users.noreply.github.com>
2020-01-03 04:39:12 +0000

    Update simulateGW.py

--- simulateGW.py
+++ simulateGW.py
@@ -5,25 +5,49 @@ from getPlayer import getPlayer, playerNames, getPosition
 from bonus import simulateBonus, lineups
 import numpy as np
 
-GW = 22
-SD = 10
-fixtures = [[team, list(i.items())[GW - 21][1]['opponent']] for team, i in fixtureDB.items() if
-            list(i.items())[21 - GW][1]['loc'] == 'h']
+print('\n' * 2)
 
+gw = 27
+SD = 10
+
+np.mode(1, 1, 1)
+fixtureDict = {i: [] for i in range(22, 39)}
+for GW in range(22, 39):
+    for team in fixtureDB:
+        if fixtureDB[team][list(fixtureDB[team])[GW - 22]]['loc'] == 'h':
+            fixtureDict[GW].append([team, fixtureDB[team][list(fixtureDB[team])[GW - 22]]['opponent']])
+fixtureDict[24].append(['West Ham', 'Liverpool'])
+fixtures = fixtureDict[gw]
 playerDict = {}
 for team, lineup in lineups.items():
     for player in lineup:
         playerDict[player] = 0
 
 for fixture in fixtures:
+    print(fixture[0] + '.' * (44 - len(fixture[0] + fixture[1])) + fixture[1])
+
+for fixture in fixtures:
     for player, pts in simulateBonus(fixture[0], fixture[1], SD).items():
         playerDict[player] += pts
-print(sorted(list(playerDict.items()), key=lambda i: i[1], reverse=True))
+print('\n' * 2)
+print('Most bonus points:', [(getPlayer(i[0], 'understat', 'name'), i[1]) for i in
+                             sorted(list(playerDict.items()), key=lambda i: i[1], reverse=True)[:10]])
+
+for fixture in fixtures:
+    for player, pts in simulateBonus(fixture[0], fixture[1], SD).items():
+        if player in lineups[fixture[0]]:
+            loc = 'h'
+        elif player in lineups[fixture[1]]:
+            loc = 'a'
+        playerDict[player] += simulatePts(getPosition(player, 'understat'), player, loc,
+                                          (loc == 'h') * fixture[1] + (loc == 'a') * fixture[0], SD)
 
 print('\n')
-for player in playerDict:
-    playerDict[player] += simulateFixtures(player, 30, SD)[GW - 21]
+
 for i in sorted(list(playerDict.items()), key=lambda i: i[1], reverse=True):
     print(getPlayer(i[0], 'understat', 'name') + '.' * (30 - len(getPlayer(i[0], 'understat', 'name'))) + str(i[1])[
                                                                                                           :4] + (
                       4 - len(str(i[1]))) * '0')
+
+print('Mean among top 21 assets: ' + str(
+    np.mean([i[1] for i in sorted(list(playerDict.items()), key=lambda i: i[1], reverse=True)[:30]]))[:4])
